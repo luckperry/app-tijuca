4728426cf2feacf18988c0c1b8102417ed385cfb
Henrique Moreira <henriquemoreira842@gmail.com>
2017-11-01 15:53:25 -0300

    v 1.0.12

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,26 +5,26 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
 
 import { MyApp } from './app.component';
-import { HomePage } from '../pages/home/home/home';
+import { HomePage } from '../pages/modulo-viagens/home/home/home';
 import { PrincipalPage } from '../pages/principal/principal/principal';
-import { ViagensPage } from '../pages/viagens/viagens';
-import { ReceitasFornecedorPage } from '../pages/receitas/receita-fornecedor/receita-fornecedor';
-import { ReceitasQntPage } from '../pages/receitas/receitas-qnt/receitas-qnt';
-import { ReceitasFotoPage } from '../pages/receitas/receitas-foto/receitas-foto';
+import { ViagensPage } from '../pages/modulo-viagens/viagens/viagens';
+import { ReceitasFornecedorPage } from '../pages/modulo-viagens/receitas/receita-fornecedor/receita-fornecedor';
+import { ReceitasQntPage } from '../pages/modulo-viagens/receitas/receitas-qnt/receitas-qnt';
+import { ReceitasFotoPage } from '../pages/modulo-viagens/receitas/receitas-foto/receitas-foto';
 
-import { GeralPage } from '../pages/geral/geral-page/geral-page';
-import { GeralDadosViagemPage } from '../pages/geral/geral-dados-viagem/geral-dados-viagem';
-import { GeralDadosMotoristaPage } from '../pages/geral/geral-dados-motorista/geral-dados-motorista';
-import { DespesasPage } from '../pages/despesas/despesas/despesas';
+import { GeralPage } from '../pages/modulo-viagens/geral/geral-page/geral-page';
+import { GeralDadosViagemPage } from '../pages/modulo-viagens/geral/geral-dados-viagem/geral-dados-viagem';
+import { GeralDadosMotoristaPage } from '../pages/modulo-viagens/geral/geral-dados-motorista/geral-dados-motorista';
+import { DespesasPage } from '../pages/modulo-viagens/despesas/despesas/despesas';
 
-import { AbastecimentoPagamentoPage } from '../pages/abastecimento/abastecimento-pagamento/abastecimento-pagamento';
-import { AbastecimentoOdometroPage } from '../pages/abastecimento/abastecimento-odometro/abastecimento-odometro';
-import { AbastecimentoBomba1Page } from '../pages/abastecimento/abastecimento-bomba-1/abastecimento-bomba-1';
-import { AbastecimentoBomba2Page } from '../pages/abastecimento/abastecimento-bomba-2/abastecimento-bomba-2';
+import { AbastecimentoPagamentoPage } from '../pages/modulo-viagens/abastecimento/abastecimento-pagamento/abastecimento-pagamento';
+import { AbastecimentoOdometroPage } from '../pages/modulo-viagens/abastecimento/abastecimento-odometro/abastecimento-odometro';
+import { AbastecimentoBomba1Page } from '../pages/modulo-viagens/abastecimento/abastecimento-bomba-1/abastecimento-bomba-1';
+import { AbastecimentoBomba2Page } from '../pages/modulo-viagens/abastecimento/abastecimento-bomba-2/abastecimento-bomba-2';
 
-import { RotasPage } from '../pages/rotas/rotas';
-import { ArlaPagPage } from '../pages/arla-32/arla-pag/arla-pag';
-import { ArlaPostoPage } from '../pages/arla-32/arla-posto/arla-posto';
+import { RotasPage } from '../pages/modulo-viagens/rotas/rotas';
+import { ArlaPagPage } from '../pages/modulo-viagens/arla-32/arla-pag/arla-pag';
+import { ArlaPostoPage } from '../pages/modulo-viagens/arla-32/arla-posto/arla-posto';
 
 
 
--- src/pages/principal/principal/principal.ts
+++ src/pages/principal/principal/principal.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
-import { ViagensPage } from '../../viagens/viagens'; 
+import { ViagensPage } from '../../modulo-viagens/viagens/viagens'; 
 
 
 @IonicPage()
