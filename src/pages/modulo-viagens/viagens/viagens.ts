8b23748183fdf66b647cc293a3535a785f9d0575
Guilherme <guilhermealbuquerquerc@gmail.com>
2017-11-13 07:01:16 -0300

    v 1.1.7

--- src/pages/modulo-viagens/viagens/viagens.ts
+++ src/pages/modulo-viagens/viagens/viagens.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
+import { RotasAbastecimentoPage } from '../abastecimento/rotas-abastecimento/rotas-abastecimento';
 import { GeralPage } from '../geral/geral-page/geral-page';
 import { DespesasPage } from '../despesas/despesas/despesas';
 import { RotasArlaPage } from '../arla-32/rotas-arla/rotas-arla';
 import { RotasReceitasPage } from '../receitas/rotas-receitas/rotas-receitas';
-import { RotasAbastecimentoPage } from '../abastecimento/rotas-abastecimento/rotas-abastecimento';
- 
+
 @IonicPage()
 @Component({
   selector: 'page-viagens',
@@ -17,8 +17,13 @@ export class ViagensPage {
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
 
+
   ionViewDidLoad() {
     console.log('ionViewDidLoad ViagensPage');
+  }
+
+  linkAbastecimento(){
+    this.navCtrl.push(RotasAbastecimentoPage);
   }
 
   linkReceitas(){
@@ -35,10 +40,6 @@ export class ViagensPage {
 
   linkGeral(){
     this.navCtrl.push(GeralPage);
-  }
-
-  linkAbastecimento(){
-    this.navCtrl.push(RotasAbastecimentoPage);
   }
 
 }
