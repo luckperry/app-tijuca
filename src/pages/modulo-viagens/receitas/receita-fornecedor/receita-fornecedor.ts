cea900ebd2a319870a6eb4b93bd742ad0a69de64
Guilherme Albuquerque <guilhermealbuquerquerc@gmail.com>
2017-12-07 13:22:01 -0300

    v 1.2.6

--- src/pages/modulo-viagens/receitas/receita-fornecedor/receita-fornecedor.ts
+++ src/pages/modulo-viagens/receitas/receita-fornecedor/receita-fornecedor.ts
@@ -8,7 +8,19 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class ReceitasFornecedorPage {
 
+  fornecedorOrigem: string = "";
+  fornecedorDestino: string = "";
+  produto: string = "";
+  tipoPagmt: string = "";
+
   constructor(public navCtrl: NavController, public navParams: NavParams) {
   }
  
+valida() {
+    if (this.fornecedorOrigem == "" || this.fornecedorDestino == "" || this.produto == "" || this.tipoPagmt == "") {
+      return false;
+    } else {
+      return true;
+    }
+  }
 }
